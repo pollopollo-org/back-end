6db47d9d85319a2f4d1f45ecc7dc1f13bb1652df
psiphph <sifamalie@gmail.com>
2019-02-25 12:25:58 +0100

    Implemented DTOs for DummyEntity

---
+++ PolloPollo.Shared/DummyCreateUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PolloPollo.Shared
+{
+    public class DummyCreateUpdateDTO
+    {
+        [Required]
+        public string Description { get; set; }
+    }
+}
