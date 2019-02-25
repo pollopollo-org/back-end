2181874157f9cba76bd5af88a7ad0d160faa4b90
psiphph <sifamalie@gmail.com>
2019-02-25 12:06:38 +0100

    Implemented PolloPolloContext

---
+++ PolloPollo.Entities/IPolloPolloContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PolloPollo.Entities
+{
+    public interface IPolloPolloContext : IDisposable
+    {
+        DbSet<DummyEntity> Dummies { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
