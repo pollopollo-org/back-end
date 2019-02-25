8a64290e10178d8c7459d452f6280153cb9a6ce3
psiphph <sifamalie@gmail.com>
2019-02-25 12:36:40 +0100

    Created DummyRepository

--- PolloPollo.Repository/DummyRepository.cs
+++ PolloPollo.Repository/DummyRepository.cs
@@ -1,8 +1,43 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using PolloPollo.Entities;
+using PolloPollo.Shared;
 
 namespace PolloPollo.Repository
 {
-    public class Class1
+    public class DummyRepository : IDummyRepository
     {
+        private readonly PolloPolloContext _context;
+
+        public DummyRepository(PolloPolloContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DummyDTO> CreateAsync(DummyCreateUpdateDTO dummy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DummyDTO> FindAsync(int dummyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<DummyDTO> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdateAsync(DummyCreateUpdateDTO dummy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteAsync(int dummyId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
