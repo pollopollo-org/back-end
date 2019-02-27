eb6f475ad8ea0a4dcfc6645b89ba240502f8500d
Yekouri <Trashskillz@gmail.com>
2019-02-27 14:18:25 +0100

    Added logic to have a user secret connectionstring to a database & added mysql database dependency & added required services to IoC Container & updated readme with database and migration tutorial & created a test migration

---
+++ PolloPollo.Entities/Migrations/20190227115106_TestMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PolloPollo.Entities.Migrations
+{
+    public partial class TestMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dummies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dummies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dummies");
+        }
+    }
+}
