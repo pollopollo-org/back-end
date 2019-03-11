43728346ac16570288d18a7898df4f7c302da473
steinkat <christina.spost@gmail.com>
2019-03-11 11:06:17 +0100

    created initial producer and reciever files

---
+++ PolloPollo.Entities/Producer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PolloPollo.Entities
+{
+    public partial class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        [Required]
+        public string Wallet { get; set; }
+    }
+}
---
+++ PolloPollo.Entities/Reciever.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PolloPollo.Entities
+{
+    public partial class DummyEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+    }
+}
