3ecba5ed2c1d5109f15028513bbe4cf99d175440
Yekouri <Trashskillz@gmail.com>
2019-03-11 13:35:42 +0100

    Added regular expressions and length limitations on the DTO & added reflection tests for annotations

--- PolloPollo.Shared/UserCreateDTO.cs
+++ PolloPollo.Shared/UserCreateDTO.cs
@@ -4,18 +4,29 @@ namespace PolloPollo.Shared
 {
     public class UserCreateDTO
     {
+        [RegularExpression(@"\S+")]
+        [StringLength(255, MinimumLength = 1)]
         [Required]
         public string FirstName { get; set; }
 
+        [RegularExpression(@"\S+")]
+        [StringLength(255, MinimumLength = 1)]
         [Required]
         public string SurName { get; set; }
 
+        // Check to match regular email pattern something@domain.com
+        [RegularExpression(@".+[@].+[.].+")]
+        [StringLength(255, MinimumLength = 1)]
         [Required]
         public string Email { get; set; }
 
+        // Countries only contains characters.
+        [RegularExpression(@"[^0-9]+")]
+        [StringLength(255, MinimumLength = 1)]
         [Required]
         public string Country { get; set; }
 
+        [StringLength(255, MinimumLength = 1)]
         [Required]
         public string Password { get; set; }
     }
