70c411df285c9c1228f705c8b175fddbb2fe40d8
steinkat <christina.spost@gmail.com>
2019-03-11 11:01:21 +0100

    Created initial user entity

---
+++ PolloPollo.Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PolloPollo.Entities
+{
+    public partial class DummyEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
