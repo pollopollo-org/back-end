37ab973e8e85b08c4b7a247fabf10678a7b128f1
Trine Borre Jørgensen <trineborre@Trines-MacBook-Air.local>
2019-03-11 11:51:08 +0100

    created dto's / create update

--- PolloPollo.Shared/ReceiverCreateUpdateDTO.cs
+++ PolloPollo.Shared/ReceiverCreateUpdateDTO.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations; 
 
 namespace PolloPollo.Shared
 {
     class ReceiverCreateUpdateDTO
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
     }
 }
