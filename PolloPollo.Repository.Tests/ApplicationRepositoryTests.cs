eb2c90a2dded16bf6808cbf46cdab327b5e580f2
psiphph <sifamalie@gmail.com>
2019-04-08 12:07:43 +0200

    Unit tested create method in applications repo

--- PolloPollo.Repository.Tests/ApplicationRepositoryTests.cs
+++ PolloPollo.Repository.Tests/ApplicationRepositoryTests.cs
@@ -1,13 +1,190 @@
 ﻿using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using PolloPollo.Entities;
+using PolloPollo.Shared;
+using PolloPollo.Shared.DTO;
+using System;
 using System.Data.Common;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace PolloPollo.Services.Tests
 {
     public class ApplicationRepositoryTests
     {
+        [Fact]
+        public async Task CreateAsync_given_null_returns_Null()
+        {
+            using (var connection = await CreateConnectionAsync())
+            using (var context = await CreateContextAsync(connection))
+            {
+                var repository = new ApplicationRepository(context);
+
+                var result = await repository.CreateAsync(default(ApplicationCreateDTO));
+
+                Assert.Null(result);
+            }
+        }
+
+        [Fact]
+        public async Task CreateAsync_given_empty_DTO_returns_Null()
+        {
+            using (var connection = await CreateConnectionAsync())
+            using (var context = await CreateContextAsync(connection))
+            {
+                var repository = new ApplicationRepository(context);
+
+                var applicationDTO = new ApplicationCreateDTO
+                {
+                    //Nothing
+                };
+
+                var result = await repository.CreateAsync(applicationDTO);
+
+                Assert.Null(result);
+            }
+        }
+
+        [Fact]
+        public async Task CreateAsync_given_invalid_DTO_returns_Null()
+        {
+            using (var connection = await CreateConnectionAsync())
+            using (var context = await CreateContextAsync(connection))
+            {
+                var repository = new ApplicationRepository(context);
+
+                var applicationDTO = new ApplicationCreateDTO
+                {
+                    Motivation = "This is not a very good motivation.",
+                };
+
+                var result = await repository.CreateAsync(applicationDTO);
+
+                Assert.Null(result);
+            }
+        }
+
+        [Fact]
+        public async Task CreateAsync_given_DTO_returns_DTO()
+        {
+            using (var connection = await CreateConnectionAsync())
+            using (var context = await CreateContextAsync(connection))
+            {
+                var repository = new ApplicationRepository(context);
+
+                var id = 1;
+
+                var user = new User
+                {
+                    Id = id,
+                    Email = "test@itu.dk",
+                    Password = "1234",
+                    FirstName = "test",
+                    SurName = "test",
+                    Country = "DK"
+                };
+
+                var userEnumRole = new UserRole
+                {
+                    UserId = id,
+                    UserRoleEnum = UserRoleEnum.Receiver
+                };
+
+                var product = new Product
+                {
+                    Id = 42,
+                    Title = "5 chickens",
+                    UserId = 1,
+                    Price = 42,
+                    Description = "Test",
+                    Location = "Test",
+                    Country = "Test",
+                };
+
+                context.Users.Add(user);
+                context.UserRoles.Add(userEnumRole);
+                context.Products.Add(product);
+                await context.SaveChangesAsync();
+
+                var applicationDTO = new ApplicationCreateDTO
+                {
+                    UserId = user.Id,
+                    ProductId = product.Id,
+                    Motivation = "Test",
+                    TimeStamp = new DateTime(2019, 04, 08),
+                };
+
+                var result = await repository.CreateAsync(applicationDTO);
+
+                Assert.Equal(applicationDTO.UserId, result.UserId);
+                Assert.Equal(applicationDTO.ProductId, result.ProductId);
+                Assert.Equal(applicationDTO.Motivation, result.Motivation);
+                Assert.Equal(applicationDTO.TimeStamp, result.TimeStamp);
+                Assert.Equal(ApplicationStatus.Open, result.Status);
+            }
+        }
+
+        [Fact]
+        public async Task CreateAsync_given_DTO_returns_DTO_with_Id()
+        {
+            using (var connection = await CreateConnectionAsync())
+            using (var context = await CreateContextAsync(connection))
+            {
+                var repository = new ApplicationRepository(context);
+
+                var id = 1;
+
+                var user = new User
+                {
+                    Id = id,
+                    Email = "test@itu.dk",
+                    Password = "1234",
+                    FirstName = "test",
+                    SurName = "test",
+                    Country = "DK"
+                };
+
+                var userEnumRole = new UserRole
+                {
+                    UserId = id,
+                    UserRoleEnum = UserRoleEnum.Receiver
+                };
+
+                var product = new Product
+                {
+                    Id = 42,
+                    Title = "5 chickens",
+                    UserId = 1,
+                    Price = 42,
+                    Description = "Test",
+                    Location = "Test",
+                    Country = "Test",
+                };
+
+                context.Users.Add(user);
+                context.UserRoles.Add(userEnumRole);
+                context.Products.Add(product);
+                await context.SaveChangesAsync();
+
+                var applicationDTO = new ApplicationCreateDTO
+                {
+                    UserId = user.Id,
+                    ProductId = product.Id,
+                    Motivation = "Test",
+                    TimeStamp = new DateTime(2019, 04, 08),
+                };
+
+                var result = await repository.CreateAsync(applicationDTO);
+
+                var expectedId = 1;
+
+                Assert.Equal(expectedId, result.ApplicationId);
+            }
+        }
+
+
+
+
 
         //Below are internal methods for use during testing
         private async Task<DbConnection> CreateConnectionAsync()
