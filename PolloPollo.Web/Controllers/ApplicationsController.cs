239b00356a7b18976d6e90043c8b8206425aa2db
Trine Borre Jørgensen <trineborre@Trines-MacBook-Air.local>
2019-04-08 12:07:53 +0200

    application controller method for post, and tests for this

--- PolloPollo.Web/Controllers/ApplicationsController.cs
+++ PolloPollo.Web/Controllers/ApplicationsController.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using PolloPollo.Services;
+using PolloPollo.Shared.DTO;
+using PolloPollo.Shared;
 
 namespace PolloPollo.Web.Controllers
-{
+{   
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ApplicationsController : ControllerBase
@@ -34,9 +39,28 @@ namespace PolloPollo.Web.Controllers
         }
 
         // POST: api/Applications
+        [ProducesResponseType(typeof(ProductDTO), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<ApplicationDTO>> Post([FromBody] ApplicationCreateDTO dto)
         {
+            var claimRole = User.Claims.First(c => c.Type == ClaimTypes.Role);
+
+            if (!claimRole.Value.Equals(UserRoleEnum.Receiver.ToString()))
+            {
+                return Unauthorized(); 
+            }
+
+            var created = await _applicationRepository.CreateAsync(dto); 
+
+            if (created == null) 
+            {
+                return Conflict(); 
+            }
+
+            return CreatedAtAction(nameof(Get), new {id = created.ApplicationId}, created); 
+
         }
 
         // PUT: api/Applications/5
