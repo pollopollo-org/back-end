80d3fb3eb6f86f6af52cf0590e934b12119f26ba
psiphph <sifamalie@gmail.com>
2019-04-08 12:51:42 +0200

    Implemented unit tests for find method in application repo

--- PolloPollo.Repository.Tests/ApplicationRepositoryTests.cs
+++ PolloPollo.Repository.Tests/ApplicationRepositoryTests.cs
@@ -182,6 +182,86 @@ namespace PolloPollo.Services.Tests
             }
         }
 
+        [Fact]
+        public async Task FindAsync_given_existing_Id_returns_ProductDTO()
+        {
+            using (var connection = await CreateConnectionAsync())
+            using (var context = await CreateContextAsync(connection))
+            {
+
+                var id = 1;
+
+                var user = new User
+                {
+                    Id = id,
+                    Email = "test@itu.dk",
+                    Password = "1234",
+                    FirstName = "test",
+                    SurName = "test",
+                    Country = "DK"
+                };
+
+                var userEnumRole = new UserRole
+                {
+                    UserId = id,
+                    UserRoleEnum = UserRoleEnum.Receiver
+                };
+
+                var product = new Product
+                {
+                    Id = id,
+                    Title = "5 chickens",
+                    UserId = 1,
+                    Price = 42,
+                    Description = "Test",
+                    Location = "Test",
+                    Country = "Test",
+                };
+
+                context.Users.Add(user);
+                context.UserRoles.Add(userEnumRole);
+                context.Products.Add(product);
+                await context.SaveChangesAsync();
+
+                var entity = new Application
+                {
+                    UserId = id,
+                    ProductId = id,
+                    Motivation = "Test",
+                    TimeStamp = new DateTime(2019, 04, 08),
+                    Status = ApplicationStatus.Open
+                };
+
+                context.Applications.Add(entity);
+                await context.SaveChangesAsync();
+
+                var repository = new ApplicationRepository(context);
+
+                var application = await repository.FindAsync(entity.Id);
+
+                Assert.Equal(entity.Id, application.ApplicationId);
+                Assert.Equal(entity.UserId, application.UserId);
+                Assert.Equal(entity.ProductId, application.ProductId);
+                Assert.Equal(entity.Motivation, application.Motivation);
+                Assert.Equal(entity.TimeStamp, application.TimeStamp);
+                Assert.Equal(entity.Status, application.Status);
+            }
+        }
+
+        [Fact]
+        public async Task FindAsync_given_nonExisting_Id_returns_null()
+        {
+            using (var connection = await CreateConnectionAsync())
+            using (var context = await CreateContextAsync(connection))
+            {
+                var repository = new ApplicationRepository(context);
+
+                var result = await repository.FindAsync(42);
+
+                Assert.Null(result);
+            }
+        }
+
 
 
 
