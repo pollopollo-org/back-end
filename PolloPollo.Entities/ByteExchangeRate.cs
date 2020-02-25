d7cf241a9a4c5fbd6867623045acce27960e25ec
Yekouri <Trashskillz@gmail.com>
2020-02-25 22:15:11 +0100

    Added ExchangeRates Entity

--- PolloPollo.Entities/ByteExchangeRate.cs
+++ PolloPollo.Entities/ByteExchangeRate.cs
@@ -3,29 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PolloPollo.Entities
 {
-    public partial class Contracts
+    public partial class ByteExchangeRate
     {
         [Key]
-        public int ApplicationId { get; set; }
+        public int Id { get; set; }
 
-        public DateTime? CreationTime { get; set; }
-
-        public int? Completed { get; set; }
-
-        public string ConfirmKey { get; set; }
-
-        public string SharedAddress { get; set; }
-
-        public string DonorWallet { get; set; }
-
-        public string DonorDevice { get; set; }
-
-        public string ProducerWallet { get; set; }
-
-        public string ProducerDevice { get; set; }
-
-        public int? Price { get; set; }
-
-        public int? Bytes { get; set; }
+        public decimal GBYTE_USD { get; set; }
     }
 }
