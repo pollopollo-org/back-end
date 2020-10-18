e4be5b70838f21c13bc975b4cecc26a733220881
raizor <raizor@gmail.com>
2020-10-18 15:38:30 +0100

    Add aaDonationConfirmed endpoint to DonorsController

--- PolloPollo.Web/Controllers/DonorsController.cs
+++ PolloPollo.Web/Controllers/DonorsController.cs
@@ -7,6 +7,7 @@ using PolloPollo.Shared.DTO;
 using PolloPollo.Web.Security;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using PolloPollo.Shared;
 
 namespace PolloPollo.Web.Controllers
 {
@@ -16,15 +17,17 @@ namespace PolloPollo.Web.Controllers
     public class DonorsController : ControllerBase
     {
         private readonly IDonorRepository _donorRepository;
+        private readonly IApplicationRepository _applicationRepository;
         private readonly ILogger<ApplicationsController> _logger;
 
-        public DonorsController(IDonorRepository dRepo, ILogger<ApplicationsController> logger)
+        public DonorsController(IDonorRepository dRepo, IApplicationRepository aRepo, ILogger<ApplicationsController> logger)
         {
+            _applicationRepository = aRepo;
             _donorRepository = dRepo;
             _logger = logger;
         }
 
-        // PUT api/applications
+        // PUT api/donors/aaDonorDeposited
         //[ApiExplorerSettings(IgnoreApi = true)]
         [AllowAnonymous]
         [HttpPut("aaDonorDeposited")]
@@ -44,10 +47,72 @@ namespace PolloPollo.Web.Controllers
                 return Ok();
             }
 
-            return NoContent();            
+            return NotFound();            
         }
 
-        // GET: api/product
+        // TODO: ** write test for this ** 
+        // PUT api/donors/aaDonationConfirmed
+        //[ApiExplorerSettings(IgnoreApi = true)]
+        [AllowAnonymous]
+        [HttpPut("aaDonationConfirmed")]
+        public async Task<IActionResult> Put([FromBody] string applicationUnitId)
+        {
+            // Only allow updates from local communicaton as only the chat-bot should report
+            // application creation results.
+            if (!HttpContext.Request.IsLocal())
+            {
+                return Forbid();
+            }
+
+            ApplicationDTO application  = await _applicationRepository.FindByUnitAsync(applicationUnitId);
+            if (application == null)
+            {
+                _logger.LogError($"Updating status of application with unit-id {applicationUnitId} failed. Application not found.");
+
+                return NotFound();
+            }
+            
+            ApplicationUpdateDTO applicationUpdateDto = new ApplicationUpdateDTO()
+            {
+                ApplicationId = application.ApplicationId,
+                UnitId = applicationUnitId,
+                Status = ApplicationStatusEnum.Completed
+            };
+
+            var(result, (emailSent, emailError)) = await _applicationRepository.UpdateAsync(applicationUpdateDto);
+
+            if (!result)
+            {
+                _logger.LogError($"Updating status of application with unit-id {applicationUnitId} failed. Application found.");
+
+                return NotFound();
+            }
+
+            _logger.LogInformation($"Status of application with id {application.ApplicationId} was updated to: {applicationUpdateDto.Status.ToString()}.");
+
+            if (applicationUpdateDto.Status == ApplicationStatusEnum.Pending)
+            {
+                _logger.LogInformation($"Email donation received to receiver, sent to localhost:25. Status: {emailSent}");
+
+                if (emailError != null)
+                {
+                    _logger.LogError($"Email error on donation received with id: {application.ApplicationId} with error message: {emailError}");
+                }
+            }
+            if (applicationUpdateDto.Status == ApplicationStatusEnum.Completed)
+            {
+                _logger.LogInformation($"Email thank you, sent to localhost:25. Status: {emailSent}");
+
+                if (emailError != null)
+                {
+                    _logger.LogError($"Email error on thank you with applicationId: {application.ApplicationId} with error message: {emailError}");
+                }
+            }
+
+            return Ok();
+        }
+
+        // GET: api/donors/donorBalance
         [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
         [AllowAnonymous]
         [HttpGet("donorBalance/{aaDonorAccount}")]
