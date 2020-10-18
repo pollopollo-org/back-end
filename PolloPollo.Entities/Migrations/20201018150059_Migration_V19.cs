9142bb34d289d8d3aa4e2abc3b7eb6852106427a
raizor <raizor@gmail.com>
2020-10-18 16:01:37 +0100

    Add Migration_V19

---
+++ PolloPollo.Entities/Migrations/20201018150059_Migration_V19.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PolloPollo.Entities.Migrations
+{
+    public partial class Migration_V19 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WalletAddress",
+                table: "Donors",
+                maxLength: 34,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 34,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Donors",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 64);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UnitId",
+                table: "Applications",
+                maxLength: 44,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnitId",
+                table: "Applications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WalletAddress",
+                table: "Donors",
+                maxLength: 34,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 34);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Donors",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 64,
+                oldNullable: true);
+        }
+    }
+}
